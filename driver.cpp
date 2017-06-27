d262d1daf7474f5a72a19aa156500c37b234a3f4
Jeremy Caldwell <jcaldwell42@gmail.com>
2017-06-26 20:35:41 -0400

    Added preliminary input checking from file

--- driver.cpp
+++ driver.cpp
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
             done = strDone == "y" ? 1 : 0;
         }
     }
-    else if(!strcmp(argv[1], "-f") && argc == 3) {  //strcmp returns 0 on equality
+    else if(!strcmp(argv[1], "-f") && argc >= 3) {  //strcmp returns 0 on equality
         std::string line;
         char cLine[10]; 
         char* lineTok;
@@ -46,10 +46,24 @@ int main(int argc, char* argv[]) {
             while(getline(input, line)) {
                 strcpy(cLine, line.c_str());
                 lineTok = strtok(cLine, " ,");
+                if(lineTok == NULL) {
+                    std::cerr << "Malformed Line. Line is: " << line << std::endl;
+                    continue; //line is malformed, skip to reading next line
+                }
                 x = atoi(lineTok);
                 lineTok = strtok(NULL, " ,");
+                if(lineTok == NULL) {
+                    std::cerr << "Malformed Line. Line is: " << line << std::endl;
+                    continue; //line is malformed, skip to reading next line
+                }
+
                 y = atoi(lineTok);
-                startLocs->push_back(x + y*COLS);
+                if(x >= 0 && x <= COLS - 1 && y >= 0 && y <= ROWS - 1) {
+                    startLocs->push_back(x + y*COLS);
+                }
+                else {
+                    std::cerr << x << ", " << y << " is an invalid index. Moving to next line..." << std::endl;
+                }
 
             }
         }
